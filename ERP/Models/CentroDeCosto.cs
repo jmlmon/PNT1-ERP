0a39bfdf7b97eb6611a699e92cfa465741ddc02e
jmlmon <juanmontesinodiaz@hotmail.com>
2022-07-26 00:19:41 -0300

    Actualización del GitIgnore y otras cositas mínimas

--- ERP/Models/CentroDeCosto.cs
+++ ERP/Models/CentroDeCosto.cs
@@ -2,8 +2,17 @@
 {
 	public class CentroDeCosto
 	{
+		#region Properties
+
 		public string Nombre { get; set; }
 		public float MontoMaximo{ get; set; }
+
+		#endregion
+
+		#region NavProperties
+
 		public List<Gasto> Gastos { get; set; }
+
+		#endregion
 	}
 }
--- ERP/Models/Empleado.cs
+++ ERP/Models/Empleado.cs
@@ -2,18 +2,27 @@
 {
 	public class Empleado
 	{
+		#region Properties
+
 		public int Id { get; set; }
 		public string Nombre { get; set; }
 		public string Apellido { get; set; }
 		public int DNI { get; set; }
-		public List<Telefono> Telefonos { get; set; }
-		public Direccion Direccion { get; set; }
 		public DateTime FechaAlta { get; set; }
 		public string Email { get; set; }
 		public ObraSocial ObraSocial { get; set; }
 		public string Legajo { get; set; }
 		public bool EmpleadoActivo { get; set; }
+
+		#endregion
+
+		#region NavProperties
+
+		public List<Telefono> Telefonos { get; set; }
+		public Direccion Direccion { get; set; }
 		public Posicion Posicion { get; set; }
 		public Imagen Foto { get; set; }
+
+		#endregion
 	}
 }
